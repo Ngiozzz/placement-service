93ec85004256af8977e2d9ec0712de809ed42594
unknown <nicole.mulela@strathmore.edu>
2021-04-20 14:19:15 +0300

    added bootstrap link

--- landingpage.php
+++ landingpage.php
@@ -5,8 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 <body>
-    
+    <div class='landing-nav'>
+        <ul class='nav nav-pills'></ul>
+            <li class="nav-item">
+                <a href="landingpage.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="hostlogin.php">Host Login</a>
+            </li>
+            <li class="nav-item">
+                <a href="hostsignup.php">Host Signup</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="adminlogin.php">Admin Portal</a></li>
+                    <li><a class="dropdown-item" href="vetterlogin.php">Vetter Portal</a></li>
+                    <li><a class="dropdown-item" href="counsellorlogin.php">Counsellor Portal</a></li>
+                     
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="aboutus.php">About us</a>
+            </li>
+            <li class="nav-item">
+                <a href="contactus.php">Contact us</a>
+            </li>
+    </div>
 </body>
 </html>
